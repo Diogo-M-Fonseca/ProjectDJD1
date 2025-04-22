a111e2f1b925d6c3530ed36a8c8377651b078333
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-04-22 15:52:28 +0100

    Level locking prototype

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField]
+    private Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnclockedLevel", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
